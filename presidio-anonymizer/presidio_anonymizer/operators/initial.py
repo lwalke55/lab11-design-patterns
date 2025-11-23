60607061c542f60467f2af52b59f56dc8c9b81ee
Laron Walker <233176084+lwalke55@users.noreply.github.com>
2025-11-23 02:45:10 +0000

    added another test and refactored initial.py

--- presidio-anonymizer/presidio_anonymizer/operators/initial.py
+++ presidio-anonymizer/presidio_anonymizer/operators/initial.py
@@ -6,7 +6,23 @@ class Initial(Operator):
 
     def operate(self, text: str = None, params: Dict = None) -> str:
         """:return: an empty value."""
-        return "initial"
+        textarr = text.split()
+        initials = ""
+        for i in textarr:
+            #if i[0][0] == char:
+            counter = 0
+            for j in textarr[len(textarr) - 1][counter]:
+                if j.isalnum():
+                    if i == textarr[len(textarr) - 1]:
+                        initials += i[0] + "."
+                    else:
+                        initials += i[0] + ". "
+                counter += 1
+                if counter == textarr[len(textarr) - 1]:
+                    break
+        initials = initials.upper()
+
+        return initials
 
     def validate(self, params: Dict = None) -> None:
         """initial does not require any parameters so no validation is needed."""
